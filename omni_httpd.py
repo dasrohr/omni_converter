56b6057e0c6e3399ad6a0877cd57fa17b74e8780
dasrohr <toni.warnecke@mailbox.org>
2017-12-21 12:26:20 +1300

    updated code according to pylint standard

--- omni_httpd.py
+++ omni_httpd.py
@@ -1,37 +1,37 @@
 #!/usr/bin/env python
-"""
-simple HTTP server in python
-proccesses GET and uses HTTP Path and pass to a celery worker
-"""
+""" simple HTTP server in python
+proccesses GET and uses HTTP Path and pass to a celery worker """
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
-import SocketServer
-from omni_tasks import load, ytie
 from celery import chain
+from omni_tasks import load, ytie
 
-class S(BaseHTTPRequestHandler):
+class Server(BaseHTTPRequestHandler):
+    """ define the http-handlers """
+    def _set_headers(self):
+        """ build http-200 response """
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
 
-  def _set_headers(self):
-    self.send_response(200)
-    self.send_header('Content-type', 'text/html')
-    self.end_headers()
+    def do_GET(self):
+        """ define the GET handler """
+        # building HTTP Header and extract path from it
+        self._set_headers()
+        seife = self.path	# passing the seife
+        if seife:
+            # start the celery chain
+            chain(load.s([seife]), ytie.s()).apply_async()
 
-  def do_GET(self):
-    # building HTTP Header and extract path from it
-    self._set_headers()
-    seife = self.path	# passing the seife
-    if seife:
-      # start the celery chain
-      chain(load.s([seife]), ytie.s()).apply_async()
-
-def run(server_class=HTTPServer, handler_class=S, port=8000):
-  server_address = ('', port)
-  httpd = server_class(server_address, handler_class)
-  print('Starting httpd on port ' + port)
-  httpd.serve_forever()
+def run(server_class=HTTPServer, handler_class=Server, port=8000):
+    """ setting up the server parameter """
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print 'Starting httpd on port ' + str(port)
+    httpd.serve_forever()
 
 if __name__ == "__main__":
-  from sys import argv
-  if len(argv) == 2:
-    run(port=int(argv[1]))
-  else:
-    run()
+    from sys import argv
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
--- omni_tasks.py
+++ omni_tasks.py
@@ -1,109 +1,106 @@
 #!/usr/bin/env python
-#
+""" the task definitions for the celery worker """
 
+import datetime
 from celery import Celery
 import youtube_dl
-import datetime
 
 # create celery tasks and define communication channels - we are just using simple redis
-task_load = Celery('omni_convert', backend='redis://localhost', broker='redis://localhost')
-task_ytie = Celery('omni_convert', backend='redis://localhost', broker='redis://localhost')
-task_plex = Celery('omni_convert', backend='redis://localhost', broker='redis://localhost')
+TASK_LOAD = Celery('omni_convert', backend='redis://localhost', broker='redis://localhost')
+TASK_YTIE = Celery('omni_convert', backend='redis://localhost', broker='redis://localhost')
+TASK_PLEX = Celery('omni_convert', backend='redis://localhost', broker='redis://localhost')
+
+FILENAME = None
 
 ## define tasks
-# task to load a video and convert it into mp3
-@task_load.task
+@TASK_LOAD.task
 def load(url_path):
-  ## define inner functions
-  # youtube_dl filename-hook to get the filename from the downloader
-  def ydl_filename_hook(dl_process):
-    global filename
-    filename = dl_process['filename']
+    """ task to load a video and convert it into mp3 """
+    # define inner functions
+    def ydl_filename_hook(dl_process):
+        """ youtube_dl filename-hook to get the filename from the downloader """
+        global FILENAME
+        FILENAME = dl_process['filename']
 
-  ## modify the ydl_options
-  # skip the download (dry-run)
-  def disable_download():
-    global do_download
-    do_download = None
-    ydl_options.update({'skip_download' : 'true'})
+    ## modify the ydl_options
+    def disable_download():
+        """ skip the download (dry-run) """
+        ydl_options.update({'skip_download' : 'true'})
 
-  # enable debug
-  def enable_debug():
-    ydl_options.pop('quiet', None)
-    ydl_options.update({'verbose' : 'true'})
+    def enable_debug():
+        """ enable debug """
+        ydl_options.pop('quiet', None)
+        ydl_options.update({'verbose' : 'true'})
 
-  # enable download of a whole playlist
-  def enable_list():
-    # not sure how this behaves in terms of what the hook spits out and depending on that
-    #  how to kick-off the next tasks for all the files in the list
-    print('we are loading a list ... normaly')
+    def enable_list():
+        """ enable download of a whole playlist """
+        # not sure how this behaves in terms of what the hook spits out and depending on that
+        #    how to kick-off the next tasks for all the files in the list
+        print 'we are loading a list ... normaly'
 
-  ## define variables
-  # current year, month and KW for path building
-  date_week = datetime.datetime.today().strftime("%W")
-  date_year = datetime.datetime.today().strftime("%Y")
-  date_month = datetime.datetime.today().strftime("%m")
+    ## define variables
+    # current year, month and KW for path building
+    date_week = datetime.datetime.today().strftime("%W")
+    date_year = datetime.datetime.today().strftime("%Y")
+    date_month = datetime.datetime.today().strftime("%m")
 
-  # build path to store files in as unicode so that youtube-dl is not complaining
-  # plex translates this to       /    albumartist                     /    album                      / title.ext
-  file_path = unicode("/store/omni_convert/" + date_year + "-" + date_month + "/" + date_year + "-" + date_week + '/%(title)s.%(ext)s')
+    # build path to store files in as unicode so that youtube-dl is not complaining
+    file_path = unicode("/store/omni_convert/" + date_year + "-" + date_month + \
+                "/" + date_year + "-" + date_week + '/%(title)s.%(ext)s')
 
-  # define a dict with default options for youtube_dl
-  #  append value to nested dict: ydl_opts['postprocessors'].append({'new_key2' : 'new_val2'})
-  #  append value to dict:        ydl_opts.update({'new_key2' : 'new_val2'})
-  #  remove value from dict:      ydl_opts.pop('key_12', None)
-  ydl_options = {
-    'format': 'bestaudio/best',
-    'postprocessors': [{
-      'key': 'FFmpegExtractAudio',
-      'preferredcodec': 'mp3',
-      'preferredquality': '320',
-      }],
-    'progress_hooks': [ydl_filename_hook],
-    'noplaylist': 'True',
-    'outtmpl': file_path,
-    'quiet': 'true',
-    }
-    # options for later implement when we go live
-    #  quiet : ture
-    #  verbose : true - switch with quiet
-    # WHAT HAPPENS WITH THE HOOK, WHEN WE DOWNLOAD A PLAYLIS???
+    # define a dict with default options for youtube_dl
+    #    append value to nested dict: ydl_opts['postprocessors'].append({'new_key2' : 'new_val2'})
+    #    append value to dict:                ydl_opts.update({'new_key2' : 'new_val2'})
+    #    remove value from dict:            ydl_opts.pop('key_12', None)
+    ydl_options = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '320',
+            }],
+        'progress_hooks': [ydl_filename_hook],
+        'noplaylist': 'True',
+        'outtmpl': file_path,
+        'quiet': 'true',
+        }
 
-  # convert passed unicode to string and
-  # process the passed url_path and extract the video-url and the options
-  url_path = str(url_path)
-  url_path = url_path[4:-2]
-  values = url_path.split('::')
-  url = [values[0]]
+    # convert passed unicode to string and
+    # process the passed url_path and extract the video-url and the options
+    url_path = str(url_path)
+    url_path = url_path[4:-2]
+    values = url_path.split('::')
+    url = [values[0]]
 
-  # if we got a url, than remove it from the option-array
-  if url:
-    values.remove(values[0])
+    # if we got a url, than remove it from the option-array
+    if url:
+        values.remove(values[0])
 
-    # build a dict with options we can process to change the download behaviour
-    #  if the value is passed - { 'passed_value' : 'function name' }
-    options = {'nodl': disable_download,
-               'debug': enable_debug,
-               'list': enable_list,
-              }
-    # process the passed options, if there are some
-    for option in values:
-      try:
-        options[option]()
-      except KeyError:
-        print('invalid option: ' + option)
+        # build a dict with options we can process to change the download behaviour
+        #    if the value is passed - { 'passed_value' : 'function name' }
+        options = {'nodl': disable_download,
+                   'debug': enable_debug,
+                   'list': enable_list,
+                  }
+        # process the passed options, if there are some
+        for option in values:
+            try:
+                options[option]()
+            except KeyError:
+                print 'invalid option: ' + option
 
-    # download
-    with youtube_dl.YoutubeDL(ydl_options) as ydl:
-      ydl.download(url)
+        # download
+        with youtube_dl.YoutubeDL(ydl_options) as ydl:
+            ydl.download(url)
 
-  else:
-    print('we received no url')
+    else:
+        print 'we received no url'
 
-  # return the filename of the loaded file for the text task
-  return filename
+    # return the filename of the loaded file for the text task
+    return FILENAME
 
 # task to perform the YTIE filename-cleanup
-@task_ytie.task
+@TASK_YTIE.task
 def ytie(filename):
-  print('you see me rollin ... ' + filename)
+    """ more to come ... """
+    print 'you see me rollin ... ' + filename
