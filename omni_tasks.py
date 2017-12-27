4c4181d959cf1f797cc76d9362896d411ad7a30d
dasrohr <toni.warnecke@mailbox.org>
2017-12-28 10:27:06 +1300

    debug

--- omni_tasks.py
+++ omni_tasks.py
@@ -32,7 +32,7 @@ def load(url_path):
     def ydl_filename_hook(dl_process):
         """ youtube_dl filename-hook to get the filename from the downloader """
         tmp = dl_process['filename']
-        print 'DEBUG :: dl_hook-filename\t{}'.format(tmp)
+        print tmp
         name = str(dl_process['filename'].rsplit('.', 1)[0].rsplit('/', 1)[1])    # get the filename
         if name not in filename:
             filename.append(str(name))  # if the filename is not already in filename add it
