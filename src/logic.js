25c2ca8d6cc9d45e55c7bbcc06bf78b3bf7e4d85
dasrohr <toni.warnecke@mailbox.org>
2018-04-10 13:15:23 +1200

    diable radio buttons

--- src/logic.js
+++ src/logic.js
@@ -2,18 +2,20 @@ function useURL() {
   var state = document.getElementById("url").value.length > 0;
   document.getElementById("vid").disabled = state;
   document.getElementById("pid").disabled = state;
-  //document.getElementById("folder").disabled = state;
-  //document.getElementById("folder_create").disabled = state;
 }
 
 function useVID() {
   var state = document.getElementById("vid").value.length > 0;
   document.getElementById("url").disabled = state;
   document.getElementById("pid").disabled = state;
+  document.getElementById("radio_v").disabled = state;
+  document.getElementById("radio_p").disabled = state;
 }
 
 function usePID() {
   var state = document.getElementById("pid").value.length > 0;
   document.getElementById("url").disabled = state;
   document.getElementById("vid").disabled = state;
+  document.getElementById("radio_v").disabled = state;
+  document.getElementById("radio_p").disabled = state;
 }
