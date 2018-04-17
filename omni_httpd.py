0255135958a306c4a084e11644faed645eeba83e
dasrohr <toni.warnecke@mailbox.org>
2018-04-17 21:33:56 +1200

    fix path

--- omni_httpd.py
+++ omni_httpd.py
@@ -15,7 +15,7 @@ except:
 # import the configuration calues from file
 from yaml import safe_load as config_load
 from os import path
-if path.isfile()
+if path.isfile('./config.yml'):
     config = config_load(open("./config.yml"))
 else:
     exit('Missing config file')
@@ -33,7 +33,7 @@ except ImportError:
     exit('please ensure that all required modules are installed: celery, validators, hashlib, bottle & that omni_tasks.py is available')
 
 # init database connection (sqlite)
-db = database.init(config['database_file'])
+db = database.connect(config['database_file'])
 
 ''' ======================================================= '''
 
@@ -112,83 +112,9 @@ def server_static(filename):
     ''' serve files in ./src/ '''
     return static_file(filename, root='./src/')
 
-run(host="0.0.0.0", port=4000)
+@route('/src/status.html')
+@auth_basic(check_cred)
+def status():
+    return template('./src/status.html', name = 'bauer')
 
-###########################
-#
-#class Server(BaseHTTPRequestHandler):
-#    """ define the http-handlers """
-##    def _set_headers(self, is_url):
-##        """ build and send http response """
-##        # if url is valid, 200 OK
-##        if is_url:
-##            self.send_response(200)
-##        # else 400 Bad Request
-##        else:
-##            self.send_response(400)
-##        self.send_header('Content-type', 'text/html')
-##        self.end_headers()
-#
-##    def do_POST(self):
-##        length = int(self.headers.getheader('content-length'))
-##        field_data = self.rfile.read(length)
-##        fields = urlparse.parse_qs(field_data)
-##        print length
-##        print field_data
-##        print fields
-#
-#    def do_GET(self):
-#        """ define the GET handler """
-#        print(parse_qs(urlparse(self.path).query))
-#        print(urlparse(self.path))
-#        #field_data = self.rfile.read(length)
-#        #fields = urlparse.parse_qs(field_data)
-#        #print field_data
-#        #print fields
-#        # get the url-path
-#        seife = self.path
-#        # extract url and options from it
-#        values = seife[1:].split('::')
-#        # validate if url is a valid url
-#        is_url = validators.url(values[0])
-#
-#        app = Bottle()
-#
-#        app.route()
-##        self._set_headers(is_url)
-##        self.wfile.write("<html><body>")
-##        if is_url:
-##            self.wfile.write("<p>200</p>")
-##        else:
-##            self.wfile.write("<p>400</p>")
-##        self.wfile.write("<p>%s</p></body></html>" % seife)
-#
-#        #if is_url:
-#        #    # start the celery chain
-#        #    chain(load.s([seife]), ytie.s()).apply_async()
-#
-#@route('/note') # or @route('/note')
-#def note():
-#    return '''
-#        <form action="/note" method="post">
-#            <textarea cols="40" rows="5" name="note">
-#            Some initial text, if needed
-#            </textarea>
-#            <input value="submit" type="submit" />
-#        </form>
-#    '''
-#
-#@post('/note') # or @route('/note', method='POST')
-#def note_update():
-#    note = bottle.request.forms.get('note')
-#    # processing the content of the text frame hereV
-#
-#def run(server_class=HTTPServer, handler_class=Server, port=8000):
-#    """ setting up the server parameter """
-#    server_address = ('', port)
-#    httpd = server_class(server_address, handler_class)
-#    print 'Starting httpd on port ' + str(port)
-#    httpd.serve_forever()
-#
-#if __name__ == "__main__":
-#    run()
+run(host="0.0.0.0", port=4000)
